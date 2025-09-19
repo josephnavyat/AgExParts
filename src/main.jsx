700cceac58c1f46b06cc4450c4a3a9ba405c6e90
JosephNavyaT <JosephNavyaT@gmail.com>
2025-09-19 18:41:37 -0500

    adding stripe part 2

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,24 @@ import ProductDetail from './components/ProductDetail.jsx';
 import { CartProvider } from './components/CartContext.jsx';
 import CartPage from './components/CartPage.jsx';
 import CartPageWrapper from './components/CartPage.jsx';
+import './styles/site.css';
 import SimpleGallery from './components/SimpleGallery.jsx';
 import SuccessPage from './components/SuccessPage.jsx';
 import FailurePage from './components/FailurePage.jsx';
-import './styles/site.css';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import CartPage from './components/CartPage.jsx';
+
+const stripePromise = loadStripe('pk_test_51S4XMHBpsFVjn5cM6uD1BRgbmhvLSnfeLPMZcp4EJNQYAQrQea122tUoOAF2exUh0Qu83i8uQj5Yp5zZXlCgj0Fc00LA6gZqpZ');
+
+function CartPageWrapper() {
+  return (
+    <Elements stripe={stripePromise}>
+      <CartPage />
+    </Elements>
+  );
+}
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
