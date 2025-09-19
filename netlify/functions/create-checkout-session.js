014de25754a94d90f427f7c664bcecd49e2bc836
JosephNavyaT <JosephNavyaT@gmail.com>
2025-09-19 18:09:04 -0500

    adding stripe

---
+++ netlify/functions/create-checkout-session.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  const { cart } = JSON.parse(event.body);
+
+  // Map cart items to Stripe line_items
+  const line_items = cart.map(({ product, quantity }) => ({
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: product.name,
+      },
+      unit_amount: Math.round(product.price * 100), // price in cents
+    },
+    quantity,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items,
+    mode: 'payment',
+    success_url: 'https://agexparts.netlify.app/success',
+    cancel_url: 'https://agexparts.netlify.app/cancel',
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ url: session.url }),
+  };
+};
