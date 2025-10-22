acfa12044682a5ef7c2b63cc663bfbbc182bf748
JosephNavyaT <JosephNavyaT@gmail.com>
2025-10-21 21:36:49 -0500

    fixing product detials due to errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // Don't handle cross-origin requests (prevents serving local cached HTML for remote module imports)
+  if (new URL(event.request.url).origin !== self.location.origin) {
+    return; // let the browser handle it
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
